f4965ab2a81b952ef51df8703086964bc7876ddd
coolheaded <coolheaded79@gmail.com>
2020-09-01 01:00:48 +0100

    news api

---
+++ precache-manifest.8d849d803c814003e0e588878ce57704.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a2051b5ccf0bc517ba00f4a770cb3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c29b6986606c0d7e9a6",
+    "url": "/static/css/main.7b227ad7.chunk.css"
+  },
+  {
+    "revision": "3be801aca06893dd7c82",
+    "url": "/static/js/2.40f8d098.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.40f8d098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c29b6986606c0d7e9a6",
+    "url": "/static/js/main.ec390e9e.chunk.js"
+  },
+  {
+    "revision": "f76bbb8b006909bb2d5d",
+    "url": "/static/js/runtime-main.b98ba16f.js"
+  }
+]);
